c44cb2bc610c529c458777e43951e7e20897234c
Alejandro Cabero Guerra <alej.cabero@gmail.com>
2024-10-09 16:43:30 +0200

    Update Particle.h

--- Particle.h
+++ Particle.h
@@ -1,18 +1,17 @@
 #pragma once
+
 namespace acg
 {
-	struct Particle
-	{
+	struct Particle {
 		double m_x;
 		double m_y;
 	private:
 		double m_speed;
 		double m_direction;
-		void init();
 
+		void init();
 	public:
 		Particle();
 		void update(int interval);
 	};
 }
-
