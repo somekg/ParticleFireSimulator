83ec49161beee7ff1348b526c051c57788a3ddef
Alejandro Cabero Guerra <alej.cabero@gmail.com>
2024-10-09 16:31:53 +0200

    Cleaned main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,5 @@
-// Particles.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <windows.h>
 #include <SDL.h>
 #include <math.h>
 #include <stdlib.h>
@@ -20,36 +18,32 @@ int main(int argc, char* argv[]) {
     if (screen.init() == false) {
         cout << "Error initialising SDL..." << endl;
     }
-
+    
     Swarm swarm;
 
     while (true) {
-
         int elapsed = SDL_GetTicks();
-       
-        swarm.update(elapsed);
-                
-
-        unsigned char red = (unsigned char)((1 + cos(elapsed * 0.01)) * 128);
-        unsigned char green = (unsigned char)((1 + sin(elapsed * 0.3)) * 128);
-        unsigned char blue = (unsigned char)((1 + cos(elapsed * 0.001)) * 128);
-
-
         
+        swarm.update(elapsed);
+       
+        unsigned char red = (unsigned char)((1 + cos(elapsed * 0.0003 )) * 128);
+        unsigned char green = (unsigned char)((1 + sin(elapsed * 0.0008)) * 128);
+        unsigned char blue = (unsigned char)((1 + cos(elapsed * 0.0001 )) * 128);
 
         const Particle* const pParticles = swarm.getParticles();
 
         for (int i = 0; i < swarm.NPARTICLES; i++) {
             Particle particle = pParticles[i];
+
             int x = (particle.m_x + 1) * Screen::SCREEN_WIDTH / 2;
             int y = particle.m_y  * Screen::SCREEN_WIDTH / 2 + Screen::SCREEN_HEIGHT/2;
+
             screen.setPixel(x, y, red, green, blue);
-
         }
+   
         screen.boxBlur();
-
         screen.update();
-
+        
         if (screen.processEvents() == false) {
             break;
         }
@@ -59,14 +53,3 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
