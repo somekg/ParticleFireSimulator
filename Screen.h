4d961fef6435a628abf1fe8f9ec52e445f5248a7
Alejandro Cabero Guerra <alej.cabero@gmail.com>
2024-10-09 16:34:29 +0200

    Cleaned Screen.h

--- Screen.h
+++ Screen.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <SDL.h>
+
 namespace acg
 {
-		class Screen
-	{
+		class Screen {
 	public:
 		const static int SCREEN_WIDTH = 800;
 		const static int SCREEN_HEIGHT = 600;
@@ -11,19 +11,19 @@ namespace acg
 		SDL_Window* m_window;
 		SDL_Renderer* m_renderer;
 		SDL_Texture* m_texture;
+
 		Uint32* m_buffer1;
 		Uint32* m_buffer2;
-
 	public:
 		Screen();
+
 		bool init();
 		bool processEvents();
+
 		void update();
 		void setPixel(int x, int y, Uint8 red, Uint8 green, Uint8 blue);
 		void close();
 		void clear();
 		void boxBlur();
-
 	};
 }
-
